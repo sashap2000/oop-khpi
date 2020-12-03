982976b9bf45fa061947efd4d4daed17f69462f4
Alexander Sharma <Ashrm@localhost.localdomain>
2020-12-03 20:36:06 +0200

    Lab06-fixed

---
+++ oleksandr_sharma/src/sharma06/Main.java
@@ -0,0 +1,9 @@
+//package sharma06;
+
+import java.io.IOException;
+public class Main {
+
+	public static void main(final String args[]) throws ClassNotFoundException, IOException {
+		Helper.start();
+	}
+}
