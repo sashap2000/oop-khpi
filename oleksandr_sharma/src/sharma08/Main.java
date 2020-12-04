1c22385a4c9dd5bd8fb796b98a553ff0badaee52
Alexander Sharma <Ashrm@localhost.localdomain>
2020-12-04 13:02:48 +0200

    Lab08

---
+++ oleksandr_sharma/src/sharma08/Main.java
@@ -0,0 +1,11 @@
+//package sharma08;
+
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(final String args[]) throws ClassNotFoundException, IOException{
+		Helper.lets_go();
+	} 
+	
+}
